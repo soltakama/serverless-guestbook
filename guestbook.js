8a32ca3ea8015da951d1ed00687c7e7b6e1e95c8
soltakama <67085589+soltakama@users.noreply.github.com>
2021-10-18 01:54:52 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7dbfc0c2.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
